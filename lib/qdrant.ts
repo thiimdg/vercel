d0c56674b5a8f1c5721f1c0008af1a5a790b7cfc
thiimdg <thiagomenzotegermini@hotmail.com>
2025-10-30 12:32:15 -0300

    fixdsad

--- lib/qdrant.ts
+++ lib/qdrant.ts
@@ -8,13 +8,7 @@ if (!process.env.QDRANT_API_KEY) {
   throw new Error('QDRANT_API_KEY is not defined');
 }
 
-if (!process.env.QDRANT_COLLECTION) {
-  throw new Error('QDRANT_COLLECTION is not defined');
-}
-
 export const qdrantClient = new QdrantClient({
   url: process.env.QDRANT_URL,
   apiKey: process.env.QDRANT_API_KEY,
 });
-
-export const COLLECTION_NAME = process.env.QDRANT_COLLECTION;
